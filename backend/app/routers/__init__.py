b2ac2e60273c39ff70d235b83eb45671d43c3e7f
Glofy <dead.leaf.dance@gmail.com>
2024-10-25 21:38:40 +1100

    delete some endpoints

--- backend/app/routers/__init__.py
+++ backend/app/routers/__init__.py
@@ -1,8 +1,8 @@
 from .heartbeat import router as heartbeat_router
 from .posts import router as posts_router
-from .users import router as users_router
+# from .users import router as users_router
 # from .files import router as file_router
 
-__all__ = ("heartbeat_router", "posts_router", "users_router", )
+__all__ = ("heartbeat_router", "posts_router", )
 
-routers = (heartbeat_router, posts_router, users_router, )
+routers = (heartbeat_router, posts_router, )
